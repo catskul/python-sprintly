d5a18f7ebef87a608e64a2ea0df4330eccd20409
Andrew Somerville <andy.somerville@gmail.com>
2014-09-17 15:40:03 -0400

    another crappy commit message not explaining what changed because current dev is haphazard

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import json
 import jira
 import sprintly
 import distance
+import pprint
 
 ***REMOVED***
 ***REMOVED***
@@ -28,7 +29,7 @@ def map_users_spr_to_jir( sprintly_map, jira_map ):
 #    return map_users_spr_to_jira
 
 
-def get_jira_projects( auth ):
+def get_jir_projs( auth ):
     api_call = JIRA_API_URI + '/user/permission/search?permissions=COMMENT_ISSUE&projectKey=%s&username='%(project)
 
 
@@ -84,43 +85,136 @@ def import_jira_project( jira_project, sprintly_project=None ):
             import_jira_issue( spr_product, jira_issue )
         else:
             print "Issue %s already exists in sprintly product: %s"%(jira_issue,spr_product)
-    
 
-def test_jira_to_sprintly(jira_auth, sprintly_auth):
-    jira_client       = jira.client.JIRA( options=jira_options, basic_auth=jira_auth )
-    sprintly_client   = sprintly.Client( sprintly_auth )
+def write_out_json( file_name, data_dict ):
+    json_file = open( file_name, 'w+' )
+    json_file.write( json.dumps( data_dict, indent=4, separators=(',', ': ') ) )
+    json_file.close()
 
-    jira_projects     = map_with( jira_client.projects(),     get_key=lambda x:x.raw['name'] )
-    sprintly_products = map_with( sprintly_client.products(), get_key=lambda x:x.raw['name'] )
+def find_tag( tag_name, spr_items ):
+    for item in spr_items:
+        for tag in item.tags:
+            if tag is tag_name:
+                return item
 
-    auto_map = guess_mapping( jira_projects, sprintly_products )     
-    for (jir_proj,spr_prod) in auto_map.iteritems():
-        if spr_prod:
-            print "JIRA proj: %s mapped to %s"%(jir_proj,spr_prod)
-        else:
-            print "JIRA proj: %s not mapped. Select Sprintly Product to map to?"%(jir_proj)
-    
-    prod_map = dict(auto_map) #FIXME
-    for (jir_proj_name,spr_prod_name) in prod_map.iteritems():
 
-        jproj = jira_projects[jir_proj_name]
-        if spr_prod:
-            print "JIRA proj: %s mapped to %s"%(jir_proj_name,spr_prod_name)
-            print "Using existing sprintly product"%(spr_prod_name)
-        else:
-            print "JIRA proj: %s not mapped. Select Sprintly Product to map to?"%(jir_proj_name)
-            print "creating sprintly product"
-            sprintly_client.create_product(jir_proj_name)
-            spr_prod_name = jir_proj_name
+class JiraToSprintlyImporter():
 
-        spr_prod = sprintly_client.get_product(spr_prod_name)
-        print "Got Sprintly product %s:%s"%(spr_prod.name,spr_prod.id)
+    def __init__(self, jira_auth, sprintly_auth):
+        self.jir_client = jira.client.JIRA( options=jira_options, basic_auth=jira_auth )
+        self.spr_client = sprintly.Client( sprintly_auth )
 
-        print "Downloading jira items."
-        jissues = jira_client.search_issues('project=%s'%jproj.key)
-        for jissue in jissues:
-            if spr_prod.search_issue('tag=%s'%jissue.key):
-                print "Converting issue %s:%s"%(jissue.key,jissue.title)
+        self.jir_projs = map_with( self.jir_client.projects(), get_key=lambda x:x.raw['name'] )
+        self.spr_prods = map_with( self.spr_client.products(), get_key=lambda x:x.raw['name'] )
+
+        self.config_file_name = 'config.json'
+        self.config = {}
+
+    def write_config(self):
+        write_out_json( self.config_file_name, self.config )
+
+    def read_config(self):
+        json_file = open( self.config_file_name, 'r' )
+        config = json.load( json_file )
+        json_file.close()
+
+        return self.config
+
+    def build_mappings(self):
+        auto_map = guess_mapping( self.jir_projs, self.spr_prods )     
+        prod_map = dict(auto_map) #FIXME
+        
+        print "Product map: "
+        print json.dumps( prod_map, indent=4, separators=(',', ': ') )
+        
+        self.config['product_map'] = prod_map
+        self.write_config()
+
+        self.config['people_maps'] = {}
+        for (jir_proj_name,spr_prod_name) in prod_map.iteritems():
+            print
+            jir_proj = self.jir_projs[jir_proj_name]
+            if spr_prod_name:
+                print "JIRA proj: %s mapped to %s"%(jir_proj_name,spr_prod_name)
+                print "Using existing sprintly product %s"%(spr_prod_name)
+            else:
+                opt = raw_input( "JIRA proj: %s not mapped. [Skip/Create]"%(jir_proj_name) )
+                
+                if len(opt) and opt.lower()[0] == 's':
+                    prod_map[jir_proj_name] = False
+                    print "skipping";
+                else :
+                    prod_map[jir_proj_name] = None
+                    print "marking for creation"
+                continue
+
+            spr_prod = self.spr_prods[spr_prod_name]
+            spr_people = spr_prod.people()
+            
+            api_call = 'user/permission/search?permissions=COMMENT_ISSUE&projectKey=%s&username='%jir_proj.key
+            rq = requests.get( self.jir_client._get_url( api_call ), auth=self.jir_client._session.auth )
+            jir_users  = [ jira.client.User( self.jir_client._options, self.jir_client._session, data ) for data in json.loads( rq.content ) ]
+
+            spr_people_map = map_with( spr_people, get_key=lambda x:x.email )
+            jir_users_map  = map_with( jir_users,  get_key=lambda x:x.emailAddress )
+
+            people_map = guess_mapping( jir_users_map, spr_people_map )
+
+            pprint.pprint( "People map: " )
+            print json.dumps( people_map, indent=4, separators=(',', ': ') )
+
+            self.config['people_maps'][jir_proj_name] = people_map
+            self.write_config()
+
+        return self.config
+            
+
+    def do_import(self,projects=None,config=None) :  
+        config=self.read_config()
+
+        if projects is None:
+            projects = config['product_map']
+
+        #for (jir_proj_name,spr_prod_name) in config['product_map'].iteritems():
+        for jir_proj_name in projects:
+            spr_prod_name = config['product_map'].get(jir_proj_name,False)
+            
+            if spr_prod_name is False:
+                print "Mapping for jira proj %s says False. Skipping"%jir_proj_name
+                continue   
+                
+            spr_prod = None
+                
+            if spr_prod_name is None:
+                print "Mapping for jira proj [{name}] is Null. Creating new Sprintly product named [{name}]".format(name=jir_proj_name)
+                spr_prod = self.spr_client.create_product( jir_proj_name )
+            elif spr_prod_name not in self.spr_prods:
+                print "Mapping for jira proj [%s] is [%s], but does not exist. Creating new Sprintly product"%(jir_proj_name,spr_prod_name)
+                spr_prod = self.spr_client.create_product( spr_proj_name )
+            else:
+                print "Mapping for jira proj [%s] is [%s]"%(jir_proj_name,spr_prod_name) 
+                spr_prod = self.spr_prods[spr_prod_name]
+
+            jir_proj = self.jir_projs[jir_proj_name]
+            print "Downloading JIRA items for [%s]"%jir_proj.name
+            jir_issues = self.jir_client.search_issues('project=%s'%jir_proj.key)
+            spr_items  = spr_prod.items()
+            
+            #pitem_tag_map = map_with( pitems, get_key=lambda x: x.tag )
+            for jir_issue in jir_issues:
+                print "Issue %s: %s"%(jir_issue.key, jir_issue.raw['fields']['summary'])
+                spr_item = find_tag( jir_issue.key, spr_items )
+                if spr_item is not None:
+                    print "Spr Item found, not creating new item"
+                    continue
+
+                print "Would create item here"
+                #spr_prod.create_item( convert_to_sprintly( jir_issue ) )
+            #    print '\n'.join( [ str(item.raw)  for item in spr_prod.items() ] )
+            #    if spr_prod.search_issue('tag=%s'%jissue.key):
+            #        print "Converting issue %s:%s"%(jissue.key,jissue.title)
+
+
 
 
 
--- sprintly.py
+++ sprintly.py
@@ -18,7 +18,7 @@ class Product:
         self.client = client
         self.repr_list = ['name']
         for thing in self.raw:
-            setattr( self, thing, lambda : raw_dict[thing] )  
+            setattr( self, thing, raw_dict[thing] )  
 
     def __repr__(self):
         return '<%s.%s: name=%s>'%(self.__module__,self.__class__.__name__,self.raw['name'])
@@ -37,9 +37,11 @@ class Person:
     def __init__(self, client, raw_dict):
         self.raw    = raw_dict
         self.client = client
+        for thing in self.raw:
+            setattr( self, thing, raw_dict[thing] )  
 
     def __repr__(self):
-        return '<%s.%s: name=%s>'%(self.__module__,self.__class__.__name__,self.raw['name'])
+        return '<%s.%s: email=%s>'%(self.__module__,self.__class__.__name__,self.raw['email'])
 
 
 class Item:
@@ -49,29 +51,50 @@ class Item:
     def __init__(self, client, raw_dict):
         self.raw    = raw_dict
         self.client = client
+        for thing in self.raw:
+            setattr( self, thing, raw_dict[thing] )  
 
     def __repr__(self):
-        return '<%s.%s: id=%s>'%(self.__module__,self.__class__.__name__,self.raw['id'])
+        return '<%s.%s: id=%s, title=%s>'%(self.__module__,self.__class__.__name__,self.raw['number'], self.raw['title'])
 
 class Client:
     def __init__(self, basic_auth):
         self.basic_auth = basic_auth
     
-    def api_call(self, call, thing_type):
-        api_call = SPRINTLY_API_URI + '/' + call
-        req = requests.get(api_call, auth=self.basic_auth)
+    def api_get(self, call, thing_type):
+        api_url = SPRINTLY_API_URI + '/' + call
+        req = requests.get(api_url, auth=self.basic_auth)
+        req.raise_for_status()
+       
+        response = json.loads(req.content) 
+        if type(response) is list:
+            return [ thing_type(self, item) for item in response ]
+        else:
+            return thing_type(self, response);
+
+    def api_post(self, call, thing_type, data):
+        api_url = SPRINTLY_API_URI + '/' + call
+        req = requests.post(api_url, auth=self.basic_auth, data=data)
         req.raise_for_status()
         
-        return [ thing_type(self, item) for item in json.loads(req.content) ]
+        response = json.loads(req.content)
+        if type(response) is list:
+            return [ thing_type(self, item) for item in response ]
+        else:
+            return thing_type(self, response)
 
     def products(self):
-        return self.api_call("products.json",Product)
+        return self.api_get("products.json",Product)
+
+    def create_product(self, name):
+        data = { 'name' : name }
+        return self.api_post("products.json",Product,data)
 
     def items(self, product_id):
-        return self.api_call("%s/items.json"%product_id,Item)
+        return self.api_get("products/%s/items.json"%product_id,Item)
         
     def people(self, product_id): 
-        return self.api_call("%s/people.json"%product_id,Person)
+        return self.api_get("products/%s/people.json"%product_id,Person)
 
     #def get_spr_user_email_map(self, product):
     #    return { user['email'] : user for user in sprintly_users } 
