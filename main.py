f22549c62fa72f6300baa330d71226aae12a7324
Andrew Somerville <andy.somerville@gmail.com>
2014-09-19 02:16:22 -0400

    commenting, jira monkeypatch, multi-client + bunch more stuff

--- main.py
+++ main.py
@@ -2,36 +2,32 @@ import requests
 import json
 import jira
 import sprintly
+import jira_monkeypatch
 import distance
 import pprint
+import re
+import itertools
+
+jira_monkeypatch.monkeypatch_jira()
 
 ***REMOVED***
+sprintly_auths = {
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+}
+
 ***REMOVED***
 
 
-def get_jir_user_email_map( project, auth ):
-    api_call = JIRA_API_URI + '/user/permission/search?permissions=COMMENT_ISSUE&projectKey=%s&username='%project
-    req = requests.get( api_call, auth=auth )
-    req.raise_for_status()
-    
-    jira_users = json.loads(req.content)
-    return { user['emailAddress'] : user for user in jira_users }
-    
-
-def map_users_spr_to_jir( sprintly_map, jira_map ):
-    return { user['id'] : jira_map[user['email']]['name'] for (key,user) in sprintly_map.iteritems() if jira_map.has_key(user['email']) }    
-
-
-#def do_user_mapping( sprintly_auth, jira_auth ):
-#    
-#    sprintly_map = get_sprintly_user_email_dict_for( product, sprintly_auth )
-#    jira_map     = get_jira_user_email_dict_for( project, jira_auth )
-#    return map_users_spr_to_jira
-
-
-def get_jir_projs( auth ):
-    api_call = JIRA_API_URI + '/user/permission/search?permissions=COMMENT_ISSUE&projectKey=%s&username='%(project)
-
+def grouper(iterable, n, fillvalue=None):
+        "Collect data into fixed-length chunks or blocks"
+        # grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx
+        args = [iter(iterable)] * n
+        return itertools.izip_longest(fillvalue=fillvalue, *args)
 
 
 def guess_mapping( set_a, set_b, dist_algo=distance.jaccard ):
@@ -61,47 +57,37 @@ def guess_mapping( set_a, set_b, dist_algo=distance.jaccard ):
     return a_to_b
 
 
-def map_with( collection, key=None, get_key=None ):
-    if not get_key and key:
-        get_key = lambda x : x[key]
-
+def map_key( collection, get_key=None ):
     if not get_key:
-        raise ValueError( "Either key or get_key arg must be set" )
+        raise ValueError( "get_key arg must be set" )
 
     return { get_key(item) : item for item in collection }
+            
+            
+def multimap_multikey( collection, get_keys ):
+    key_map = {}
+    for item in collection:
+        for key in get_keys( item ):
+            if not key_map.has_key( key ):
+                key_map[key] = []
 
+            key_map[key].append( item )
 
-def import_jira_issue( sprintly_product, jira_issue ):
-    #sprintly_product
-    pass
+    return key_map
 
-def import_jira_project( jira_project, sprintly_project=None ):
-    
-    #if
-    #spr_client
-
-    for jira_issue in jira_project:
-        if not spr_project.has_issue( jira_issue ):
-            import_jira_issue( spr_product, jira_issue )
-        else:
-            print "Issue %s already exists in sprintly product: %s"%(jira_issue,spr_product)
 
 def write_out_json( file_name, data_dict ):
     json_file = open( file_name, 'w+' )
     json_file.write( json.dumps( data_dict, indent=4, separators=(',', ': ') ) )
     json_file.close()
 
-def find_tag( tag_name, spr_items ):
-    for item in spr_items:
-        for tag in item.tags:
-            if tag is tag_name:
-                return item
-
-
 
 class JiraToSprintlyConverter():
-    def __init__(self,user_map):
-        self.user_map = user_map
+    def __init__(self,jir_users, spr_people):
+        self.jir_user_map   = map_key( jir_users,  get_key=lambda x:x.emailAddress )
+        self.spr_person_map = map_key( spr_people, get_key=lambda x:x.email )
+
+        self.jir_users      = map_key( jir_users, get_key=lambda x:x.key )
 
     def convert_issuetype(self,jira_issue):
         if len(jira_issue.raw['fields']['subtasks']) > 0:
@@ -135,14 +121,23 @@ class JiraToSprintlyConverter():
     
     def lookup_person(self, email_address):
         try:
-            return self.user_map[email_address].id
+            return self.spr_person_map[email_address].id
         except KeyError:
-            return email_address
+            try:
+                jir_name = self.jir_user_map[email_address].displayName.split()
+            except KeyError:
+                return email_address 
+            return {
+                'first_name' : jir_name[0],
+                'last_name'  : jir_name[1],
+                'email'      : email_address,
+                'admin'      : False,
+            }
     
-    def convert_to_sprintly(self, jira_issue ):
+    def jira_issue_to_sprintly(self, jira_issue ):
         jira_fields = jira_issue.raw['fields']
 
-        reporter = self.lookup_person(jira_fields['reporter']['emailAddress'])
+        reporter = jira_fields['reporter']['emailAddress']
        
         spr_data = {} 
         spr_data['type']        = self.convert_issuetype( jira_issue )
@@ -156,17 +151,25 @@ class JiraToSprintlyConverter():
         spr_data['assigned_to'] = self.lookup_person( jira_fields['assignee']['emailAddress'] )
         spr_data['tags']        = ','.join( [jira_issue.key, 'reporter-%s'%reporter] )
 
-
         return spr_data
+
+    def jira_comment_to_sprintly(self, jira_comment):
+        parts = re.split( '\[~(\w*)\]', jira_comment.body ) 
+        
+        for idx in range(len(parts))[1::2]:
+            match = parts[idx]
+            spr_person = self.spr_person_map.get( self.jir_users[match].emailAddress )
+            parts[idx] = '@[%s %s](pk:%s)'%(spr_person.first_name,spr_person.last_name,spr_person.id) 
+       
+        body = ''.join(parts) 
+        return { 'body' : body }
 
 class JiraToSprintlyImporter():
 
     def __init__(self, jira_auth, sprintly_auth):
-        self.jir_client = jira.client.JIRA( options=jira_options, basic_auth=jira_auth )
-        self.spr_client = sprintly.Client( sprintly_auth )
-
-        self.jir_projs = map_with( self.jir_client.projects(), get_key=lambda x:x.raw['name'] )
-        self.spr_prods = map_with( self.spr_client.products(), get_key=lambda x:x.raw['name'] )
+        self.jir_client  = jira.client.JIRA( options=jira_options, basic_auth=jira_auth )
+        self.spr_client  = sprintly.Client( sprintly_auth )
+        self.spr_clients = { auth[0] : sprintly.Client( auth ) for auth in sprintly_auths.iteritems() }
 
         self.config_file_name = 'config.json'
         self.config = {}
@@ -182,7 +185,10 @@ class JiraToSprintlyImporter():
         return self.config
 
     def build_mappings(self):
-        auto_map = guess_mapping( self.jir_projs, self.spr_prods )     
+        jir_proj_lookoup = map_key( self.jir_client.projects(), get_key=lambda x:x.raw['name'] )
+        spr_prod_lookoup = map_key( self.spr_client.products(), get_key=lambda x:x.raw['name'] )
+        
+        auto_map = guess_mapping( jir_proj_lookoup, spr_prod_lookoup )     
         prod_map = dict(auto_map) #FIXME
         
         print "Product map: "
@@ -190,11 +196,22 @@ class JiraToSprintlyImporter():
         
         self.config['product_map'] = prod_map
         self.write_config()
+            
+        spr_people = self.spr_client.all_people() # spr_prod.people()
+        jir_users  = self.jir_client.get_all_users() # get_users(jir_proj.key)
 
-        self.config['people_maps'] = {}
+        spr_person_map = map_key( spr_people, get_key=lambda x:x.email )
+        jir_user_map  = map_key( jir_users,  get_key=lambda x:x.emailAddress )
+
+        person_map = guess_mapping( jir_user_map, spr_person_map )
+            
+        pprint.pprint( "People map: " )
+        print json.dumps( person_map, indent=4, separators=(',', ': ') )
+
+        self.config['person_maps'] = {}
         for (jir_proj_name,spr_prod_name) in prod_map.iteritems():
             print
-            jir_proj = self.jir_projs[jir_proj_name]
+            jir_proj = jir_proj_lookoup[jir_proj_name]
             if spr_prod_name:
                 print "JIRA proj: %s mapped to %s"%(jir_proj_name,spr_prod_name)
                 print "Using existing sprintly product %s"%(spr_prod_name)
@@ -209,34 +226,38 @@ class JiraToSprintlyImporter():
                     print "marking for creation"
                 continue
 
-            spr_prod = self.spr_prods[spr_prod_name]
-            spr_people = spr_prod.people()
-            
-            api_call = 'user/permission/search?permissions=COMMENT_ISSUE&projectKey=%s&username='%jir_proj.key
-            rq = requests.get( self.jir_client._get_url( api_call ), auth=self.jir_client._session.auth )
-            jir_users  = [ jira.client.User( self.jir_client._options, self.jir_client._session, data ) for data in json.loads( rq.content ) ]
+            spr_prod = spr_prod_lookoup[spr_prod_name]
 
-            spr_people_map = map_with( spr_people, get_key=lambda x:x.email )
-            jir_users_map  = map_with( jir_users,  get_key=lambda x:x.emailAddress )
-
-            people_map = guess_mapping( jir_users_map, spr_people_map )
-
-            pprint.pprint( "People map: " )
-            print json.dumps( people_map, indent=4, separators=(',', ': ') )
-
-            self.config['people_maps'][jir_proj_name] = people_map
+            self.config['person_maps'][jir_proj_name] = person_map
             self.write_config()
 
         return self.config
             
 
     def do_import(self,projects=None,config=None) :  
-        config=self.read_config()
+        if not config:
+            print "no config passed. Reading from disk"
+            config=self.read_config()
 
         if projects is None:
             projects = config['product_map']
 
-        #for (jir_proj_name,spr_prod_name) in config['product_map'].iteritems():
+        print "Retrieiving projects"
+        jir_projs = self.jir_client.projects()
+        spr_prods = self.spr_client.products()
+         
+        jir_proj_lookoup = map_key( jir_projs, get_key=lambda x:x.raw['name'] )
+        spr_prod_lookoup = map_key( spr_prods, get_key=lambda x:x.raw['name'] )
+       
+        print "Retrieving users"
+        jir_users  = self.jir_client.get_all_users(projects=jir_projs)
+        spr_people = self.spr_client.all_people(products=spr_prods) 
+            
+        jir_user_map   = map_key( jir_users,  get_key=lambda x:x.emailAddress )
+        spr_person_map = map_key( spr_people, get_key=lambda x:x.email )
+            
+        converter = JiraToSprintlyConverter( jir_users, spr_people )
+
         for jir_proj_name in projects:
             spr_prod_name = config['product_map'].get(jir_proj_name,False)
             
@@ -249,45 +270,64 @@ class JiraToSprintlyImporter():
             if spr_prod_name is None:
                 print "Mapping for jira proj [{name}] is Null. Creating new Sprintly product named [{name}]".format(name=jir_proj_name)
                 spr_prod = self.spr_client.create_product( jir_proj_name )
-            elif spr_prod_name not in self.spr_prods:
+            elif spr_prod_name not in spr_prod_lookoup:
                 print "Mapping for jira proj [%s] is [%s], but does not exist. Creating new Sprintly product"%(jir_proj_name,spr_prod_name)
                 spr_prod = self.spr_client.create_product( spr_proj_name )
             else:
                 print "Mapping for jira proj [%s] is [%s]"%(jir_proj_name,spr_prod_name) 
-                spr_prod = self.spr_prods[spr_prod_name]
+                spr_prod = spr_prod_lookoup[spr_prod_name]
 
-
-            spr_people     = spr_prod.people()
-            spr_people_map = map_with( spr_people, get_key=lambda x:x.email )
-            converter      = JiraToSprintlyConverter( spr_people_map )
-
-            jir_proj = self.jir_projs[jir_proj_name]
+            jir_proj    = jir_proj_lookoup[jir_proj_name]
             print "Downloading JIRA items for [%s]"%jir_proj.name
-            jir_issues = self.jir_client.search_issues('project=%s'%jir_proj.key)
-            spr_items  = spr_prod.items()
+            jir_issues  = self.jir_client.search_issues('project=%s'%jir_proj.key)
+            print "Downloaded %i items"%len(jir_issues)
+            spr_items   = spr_prod.items()
+            spr_tag_map = multimap_multikey( spr_items, lambda x: x.tags )
             
-            #pitem_tag_map = map_with( pitems, get_key=lambda x: x.tag )
             for jir_issue in jir_issues:
                 print "Issue %s: %s"%(jir_issue.key, jir_issue.raw['fields']['summary'])
-                spr_item = find_tag( jir_issue.key, spr_items )
-                if spr_item is not None:
-                    print "Spr Item found, not creating new item"
-                    continue
+        
+                jir_issue_tag = jir_issue.key.lower()
+                if spr_tag_map.has_key( jir_issue_tag ):
+                    assert( len(spr_tag_map[jir_issue_tag]) == 1 )
+                    spr_item = spr_tag_map[jir_issue_tag][0]
+                    print "Issue [%s] exists, using exsiting item"%jir_issue.key
 
-                print "Would create item here"
-                print "Would submit"
-                spr_issue_data = converter.convert_to_sprintly( jir_issue )
-                pprint.pprint( spr_issue_data ) 
-                if not (type(spr_issue_data['assigned_to']) is int):
-                    print "Assignee not found. invite required"
-                #spr_prod.create_item( spr_issue_data )
-            #    print '\n'.join( [ str(item.raw)  for item in spr_prod.items() ] )
-            #    if spr_prod.search_issue('tag=%s'%jissue.key):
-            #        print "Converting issue %s:%s"%(jissue.key,jissue.title)
+                else:
+                    spr_item_data = converter.jira_issue_to_sprintly( jir_issue )
+                        
+                    pprint.pprint( spr_item_data )
+                    
+                    assigned_to = spr_item_data['assigned_to'] 
+                    if isinstance(assigned_to,basestring):
+                        print "Failed to find %s. skipping issue"%assigned_to
+                        continue
 
+                    if type(assigned_to) is dict:
+                        print "Assignee not found. invite required"
+                        print spr_prod.create_person( assigned_to ).raw
 
+                    author_email = jir_issue.fields.creator.emailAddress
+                    item_spr_client = self.spr_clients.get(author_email)
+                    if not item_spr_client:
+                        print "No client for %s, using default %s"%(author_email,sprintly_auth[0])
 
+                    spr_item =  spr_prod.create_item(spr_item_data, client=item_spr_client)
 
+                jir_comments    = self.jir_client.comments( jir_issue.id )
+                spr_comments    = spr_item.comments()
+                spr_comment_map = map_key( spr_comments, get_key=lambda x: hash(x.body) )
 
+                for jir_comment in jir_comments:
+                    if spr_comment_map.has_key(hash(jir_comment.body)):
+                        print "Comment already exists"
+                    else:
+                        author_email       = jir_comment.author.emailAddress
+                        comment_spr_client = self.spr_clients.get(author_email)
+                        if not comment_spr_client:
+                            print "No client for %s, using default %s"%(author_email,sprintly_auth[0])
 
+                        spr_comment_data   = converter.jira_comment_to_sprintly(jir_comment)
+                        spr_comment        = spr_item.create_comment( spr_comment_data, client=comment_spr_client )
+                    #print spr_comment.raw
 
