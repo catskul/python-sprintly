297a950bccd18e6b0970571e5d74ecf70d1da333
Andrew Somerville <andy.somerville@gmail.com>
2014-10-08 14:51:14 -0400

    merge in changes from jira2sprintly makeing it possible to save items after changing them

--- sprintly.py
+++ sprintly.py
@@ -6,6 +6,29 @@ SPRINTLY_URI      = 'https://sprint.ly'
 SPRINTLY_API_PATH = '/api'
 SPRINTLY_API_URI  = SPRINTLY_URI + SPRINTLY_API_PATH
 
+
+def wrap(data,thing_type, client):
+    if type(data) is list:
+        return [ thing_type(client, item) for item in data ]
+    else:
+        return thing_type(cleint, data)
+
+
+class Account:
+    def __init__(self, basic_auth, fake_create=False):
+        self.client = Client( basic_auth, fake_create )
+
+    def products(self):
+        return wrap( self.client.products(), Product, self.client )
+    
+    def all_people(self,products=None):
+        products = products or self.products()
+        people = set()
+        for product in products:
+            people.update( product.people() )
+
+        return people
+
 class ApiThing:
     def __repr__(self):
         origin = '%s.%s'%(self.__module__,self.__class__.__name__)
@@ -18,6 +41,10 @@ class ApiThing:
     def __eq__(self,other):
         return self.__hash__() == other.__hash__()
 
+    def update_with(self,data_dict):
+        for thing in data_dict:
+            setattr( self, thing, data_dict[thing] )
+
 
 class Product(ApiThing):
     def __init__(self, client, raw_dict):
@@ -25,21 +52,20 @@ class Product(ApiThing):
         self.client = client
         self.repr_list = ['name','id']
         self.hash_parts = ['id']
-        for thing in self.raw:
-            setattr( self, thing, raw_dict[thing] )  
+        self.update_with( self.raw )
 
     def people(self):
-        return self.client.people(self.raw['id'])
+        return wrap( self.client.people(self.raw['id']), Person, self.client )
 
     def create_person(self,data):
-        return self.client.create_person(self.raw['id'],data)
+        return wrap( self.client.create_person(self.raw['id'],data), Person, self.client )
 
-    def items(self):
-        return self.client.items(self.raw['id'])
+    def items(self,status=None):
+        return wrap( self.client.items(self.raw['id'],status), Item, self.client )
 
     def create_item(self,data,client=None):
         client = client or self.client
-        return client.create_item(self.raw['id'],data)
+        return wrap( client.create_item(self.raw['id'],data), Item, self.client )
 
 
 class Person(ApiThing):
@@ -48,8 +74,7 @@ class Person(ApiThing):
         self.client = client
         self.repr_list = ['email','id']
         self.hash_parts = ['id']
-        for thing in self.raw:
-            setattr( self, thing, raw_dict[thing] )  
+        self.update_with( self.raw )
 
 
 class Item(ApiThing):
@@ -58,15 +83,46 @@ class Item(ApiThing):
         self.client = client
         self.repr_list = ['title','number']
         self.hash_parts = ['number']
-        for thing in self.raw:
-            setattr( self, thing, raw_dict[thing] )  
+        self.update_with( self.raw )
 
     def comments(self):
-        return self.client.comments(self.product['id'],self.number)
+        return wrap( self.client.comments(self.product['id'],self.number), Comment, self.client )
 
     def create_comment(self,data,client=None):
         client = client or self.client
-        return client.create_comment(self.product['id'],self.number,data)
+        return wrap( client.create_comment(self.product['id'],self.number,data), Comment, self.client )
+
+    def save(self):
+        fields = [
+            'title', 
+            'who', 
+            'what', 
+            'why', 
+            'description', 
+            'score', 
+            'status', 
+            'assigned_to', 
+            'tags',
+            'parent',
+        ]
+        if self.type == 'story':
+            fields.remove( 'title' )
+
+        fields = set(fields).intersection(set(self.raw))
+        
+        data = { field : getattr( self, field ) for field in fields }
+        for key in data:
+            if type(data[key]) is dict:
+                data[key] = data[key]['id']
+
+        return_data = self.client.update_item( self.product['id'], self.number, data )
+        self.raw = return_data
+        self.update_with( self.raw )
+        return self 
+
+    def delete(self):
+        return self.client.delete_item( self.product['id'], self.number )
+        
 
 
 class Comment(ApiThing):
@@ -75,27 +131,23 @@ class Comment(ApiThing):
         self.client = client
         self.repr_list = ['id']
         self.hash_parts = ['id']
-        for thing in self.raw:
-            setattr( self, thing, raw_dict[thing] )  
-    
+        self.update_with( self.raw ) 
 
 class Client:
     def __init__(self, basic_auth, fake_create=False):
         self.basic_auth = tuple(basic_auth)
         self.fake_create = fake_create
     
-    def api_get(self, call, thing_type, params=None):
+    def api_get(self, call, params=None):
         api_url = SPRINTLY_API_URI + '/' + call
         req = requests.get(api_url, auth=self.basic_auth, params=params)
         req.raise_for_status()
        
         response = json.loads(req.content) 
-        if type(response) is list:
-            return [ thing_type(self, item) for item in response ]
-        else:
-            return thing_type(self, response);
 
-    def api_post(self, call, thing_type, data, fake_data=None):
+        return response
+
+    def api_post(self, call, data, fake_data=None):
         api_url = SPRINTLY_API_URI + '/' + call
         
         if self.fake_create:
@@ -109,61 +161,67 @@ class Client:
             req.raise_for_status()
         
             response = json.loads(req.content)
+        return response
 
-        if type(response) is list:
-            return [ thing_type(self, item) for item in response ]
-        else:
-            return thing_type(self, response)
+    def api_delete(self, call, params=None):
+        api_url = SPRINTLY_API_URI + '/' + call
+        req = requests.delete(api_url, auth=self.basic_auth)
+        req.raise_for_status()
+       
+        response = json.loads(req.content) 
+
+        return response
 
     def products(self):
-        return self.api_get("products.json",Product)
+        return self.api_get("products.json")
 
     def create_product(self, name):
         data = { 'name' : name }
-        return self.api_post("products.json",Product,data)
+        return self.api_post("products.json",data)
 
-    def items(self, product_id):
+    def items(self, product_id, status=None):
         params = {
-            'status' : 'backlog,in-progress,completed,accepted',
+            'status' : 'someday,backlog,in-progress,completed,accepted',
             'limit'  : 100,
             'offset' : 0,
         }
+        params['status'] = status or params['status']
         data = []
         count = 1
         while count > 0:
-            res = self.api_get("products/%s/items.json"%product_id,Item,params=params)
+            print "getting items starting at %s"%params['offset']
+            res = self.api_get("products/%s/items.json"%product_id,params=params)
             params['offset'] += params['limit']
             data += res
             count = len( res )
 
         return data
         
+    def update_item(self, product_id, item_id, data):
+        return self.api_post("products/%s/items/%s.json"%(product_id,item_id),data)
+
     def create_item(self, product_id, data):
         fake_data = { 
             'number' : -1,
             'product' : { 'id' : product_id }
         }
-        return self.api_post("products/%s/items.json"%product_id,Item,data,fake_data=fake_data)
+        return self.api_post("products/%s/items.json"%product_id,data,fake_data=fake_data)
+
+    def delete_item(self, product_id, item_id):
+        return self.api_delete("products/%s/items/%s.json"%(product_id,item_id))
 
     def comments(self,product_id, item_number):
         if item_number == -1 or item_number == -1:
             return []
         else:
-            return self.api_get("products/%s/items/%s/comments.json"%(product_id,item_number),Comment)
+            return self.api_get("products/%s/items/%s/comments.json"%(product_id,item_number))
 
     def create_comment(self,product_id, item_number, data):
-        return self.api_post("products/%s/items/%s/comments.json"%(product_id,item_number),Comment,data)
+        return self.api_post("products/%s/items/%s/comments.json"%(product_id,item_number),data)
 
     def people(self, product_id): 
-        return self.api_get("products/%s/people.json"%product_id,Person)
+        return self.api_get("products/%s/people.json"%product_id)
 
     def create_person(self, product_id, data):
-        return self.api_post("products/%s/people.json"%product_id,Person,data)
+        return self.api_post("products/%s/people.json"%product_id,data)
 
-    def all_people(self,products=None):
-        products = products or self.products()
-        people = set()
-        for product in products:
-            people.update( product.people() )
-
-        return people
