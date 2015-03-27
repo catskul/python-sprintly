ffde87ccacbca3bc67a29707db98b082d61b65a5
Andrew Somerville <andy.somerville@gmail.com>
2015-03-26 20:05:58 -0400

    first try at setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+from pip.req import parse_requirements
+from setuptools import setup, find_packages
+
+import sprintly 
+
+install_reqs = parse_requirements('requirements.txt')
+reqs = [str(ir.req) for ir in install_reqs]
+
+setup(
+    name='sprintly',
+    version=sprintly.__version__,
+    description='API wrapper for Sprint.ly',
+    long_description=open('README.rst').read(),
+    author=sprintly.__author__,
+    url='https://github.com/catskul/python-sprintly',
+    packages=find_packages(),
+    download_url='http://pypi.python.org/pypi/sprintly/',
+    keywords='sprintly api wrapper',
+    zip_safe=True,
+    install_requires=reqs,
+    py_modules=['sprintly'],
+    classifiers=[
+        'Development Status :: 3 - Alpha'
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
+
